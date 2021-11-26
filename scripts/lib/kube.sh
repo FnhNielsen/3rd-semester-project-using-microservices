3f15b13deefd141cf0364afe0a04cc9382458ab0
Henrik Ankersø <Heank16@studten.sdu.dk>
2021-11-26 16:17:45 +0100

    Add kube_status and kube_describe

--- scripts/lib/kube.sh
+++ scripts/lib/kube.sh
@@ -32,3 +32,27 @@ function kube_delete {
 
   kubectl apply --kubeconfig="$2" -f "$1" || error "Failed to delete."
 }
+
+function kube_describe {
+  # $1: file set as file path (string) [Required]
+  # $2: config file set as file path (string) [Required]
+  debug "File: \"$1\""
+  debug "Config file: \"$2\""
+
+  kubectl describe --kubeconfig="$2" -f "$1" || error "Failed to describe."
+}
+
+function kube_status {
+  # $1: kind (string) [Required]
+  # $2: name (string) [Required]
+  # $3: config file set as file path (string) [Required]
+  # $4: timeout (string) [Optional]
+  [ -z "$4" ] && timeout=3m || timeout=$4
+
+  debug "Kind: \"$1\""
+  debug "Name: \"$2\""
+  debug "Config file: \"$3\""
+  debug "Timeout: \"$4\""
+
+  kubectl rollout status --kubeconfig="$3" --timeout="${timeout}" "$1" "$2" || error "Failed to get status."
+}
