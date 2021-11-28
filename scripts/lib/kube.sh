ed0f4d0940e164d015e6b0c89d1ceeb9fc2ea5e3
Henrik Ankersø <Heank16@studten.sdu.dk>
2021-11-28 02:37:00 +0100

    Changes describe to outcome Outcome describe all service and show a log for any pods the was deployed with the manifest

--- scripts/lib/kube.sh
+++ scripts/lib/kube.sh
@@ -34,12 +34,12 @@ function kube_delete {
 }
 
 function kube_describe {
-  # $1: file set as file path (string) [Required]
+  # $1: "<kind>/<service name>" (string) [Required]
   # $2: config; file set as file path (string) [Required]
   debug "File: \"$1\""
   debug "Config file: \"$2\""
 
-  kubectl describe --kubeconfig="$2" -f "$1" || error "Failed to describe $1."
+  kubectl describe "$1" --kubeconfig="$2" || error "Failed to describe $1."
 }
 
 function kube_status {
@@ -53,4 +53,34 @@ function kube_status {
   debug "Timeout: \"${timeout}\""
 
   kubectl rollout status --kubeconfig="$2" --timeout="${timeout}" "$1" || error "Failed to get status."
+}
+
+function kube_log {
+  # $1: pod (string) [Required]
+  # $2: container (string) [Required]
+  # $3: config; file set as file path (string) [Required]
+  debug "Pod: \"$1\""
+  debug "Container: \"$2\""
+  debug "Config file: \"$3\""
+
+  kubectl logs "$1" "$2" --kubeconfig="$3" || error "Failed to get log."
+}
+
+function kube_service_pods_log {
+  # $1: "<kind>/<service name>" (string) [Required]
+  # $2: config; file set as file path (string) [Required]
+  debug "Get pod(s) for: \"$1\""
+  debug "Config file: \"$2\""
+
+  service_json=$(kubectl get "$1" -o json --kubeconfig="$2")
+  for selector_name in $(echo "${service_json}" | jq -r ".spec.selector.matchLabels | .[]"); do
+    debug "Selector name: \"${selector_name}\""
+    for pod_name in $(kubectl get pod -o json --kubeconfig="$2" | jq -r ".items[] | select(.metadata.name? | match(\"^${selector_name}-[a-z0-9]+-[a-z0-9]+$\")) | .metadata.name"); do
+      debug "Pod name: \"${pod_name}\""
+      for container_name in $(echo "${service_json}" | jq -r ".spec.template.spec.containers[].name"); do
+        debug "Container name: \"${container_name}\""
+        kube_log "${pod_name}" "${container_name}" "$2"
+      done
+    done
+  done
 }
